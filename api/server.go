275e425875477ac6c552963388bcaa68b670a20b
x7eo <99x7eo@gmail.com>
2022-04-20 18:46:27 -0600

    login grpc

--- api/server.go
+++ api/server.go
@@ -1,24 +1,32 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/tmortx7/gapi-go-basic/db/sqlc"
 	"github.com/tmortx7/gapi-go-basic/pb"
+	"github.com/tmortx7/gapi-go-basic/token"
 	"github.com/tmortx7/gapi-go-basic/util"
 )
 
 // Server serves gRPC requests for our banking service.
 type Server struct {
 	pb.UnimplementedSimpleServer
-	config util.Config
-	store  db.Store
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
 }
 
 // NewServer creates a new gRPC server.
 func NewServer(config util.Config, store db.Store) (*Server, error) {
-
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
 	server := &Server{
-		config: config,
-		store:  store,
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
 	}
 
 	return server, nil
